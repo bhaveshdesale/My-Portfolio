3116cabcb60a838a2b39f5415a0e8f60ab621aef
Bhavesh <143746617+bhaveshdesale@users.noreply.github.com>
2025-04-22 00:24:47 +0530

    issue fix

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -125,7 +125,7 @@ export const Contact = () => {
     setButtonText("Sending...");
 
     try {
-      const response = await fetch("http://localhost:5000/contact", {
+      const response = await fetch("https://render-backend-6-xllz.onrender.com/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json;charset=utf-8",
