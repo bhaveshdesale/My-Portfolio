7bfc1dea533b8c3815ec1cc66eb42bc0d7f34be8
Bhavesh <143746617+bhaveshdesale@users.noreply.github.com>
2025-04-21 23:47:38 +0530

    Project add

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,6 +5,7 @@ import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import projImg4 from "../assets/img/project-img4.png";
 import projImg5 from "../assets/img/project-img5.png";
+import projImg6 from "../assets/img/project-img6.png";
 
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
@@ -49,13 +50,13 @@ export const Projects = () => {
       githubLink: "https://github.com/bhaveshdesale/EcoChatApp.git", // Replace with actual GitHub link
       liveLink: "https://appetize.io/app/b_fjuehxnxbdqvcn2kendl5ppv2i", // Replace with actual live link
     },
-    // {
-    //   title: "Pdf ChatBot",
-    //   description: "ChatBot for Pdf interaction",
-    //   imgUrl: projImg1,
-    //   githubLink: "https://github.com/yourusername/PdfChatBot", // Replace with actual GitHub link
-    //   liveLink: "https://yourwebsite.com/PdfChatBot", // Replace with actual live link
-    // },
+    {
+      title: "Pdf ChatBot",
+      description: "ChatBot for Pdf interaction",
+      imgUrl: projImg6,
+      githubLink: "https://github.com/bhaveshdesale/Chat-With-Pdf.git", // Replace with actual GitHub link
+      liveLink: "https://pdfchatbot-vjpbmcw9hkfyyzrkjdptel.streamlit.app/", // Replace with actual live link
+    },
   
     // {
     //   title: "Paste App",
